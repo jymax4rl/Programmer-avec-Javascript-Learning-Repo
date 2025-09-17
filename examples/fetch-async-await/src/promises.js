99d1b0895671ab85baa11eae0d395ed83947175c
jymax4rl <116975025+jymax4rl@users.noreply.github.com>
2025-09-17 08:37:59 +0200

    resolve & reject arguments

---
+++ examples/fetch-async-await/src/promises.js
@@ -0,0 +1,30 @@
+const speed = 100;
+
+
+const isDangerous = speed > 2000;
+
+
+const maPromesse = new Promise((resolve, reject) => {
+    // Traitement asynchrone
+
+    // Si le traitement réussit, on appelle resolve() avec la valeur de retour
+    if (!isDangerous) {
+        setTimeout(resolve, 2000, 'plane landed successfully!');
+    } else {
+        // Si le traitement échoue, on appelle reject() avec l'erreur
+        reject('plane crashed during landing!');
+
+    }
+});
+
+
+
+maPromesse.then(result => {
+    console.log('En attente de la promesse...');
+    console.log('Résultat de la promesse:', result);
+}).catch(error => {
+    console.error('Erreur dans la promesse:', error);
+});
+
+
+
